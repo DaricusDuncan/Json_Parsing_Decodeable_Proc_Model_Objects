c66cd6588f8e027d3e852e8482df73d33123a4d3
Daricus_Duncan <drdunkan15@gmail.com>
2017-10-02 19:41:41 -0500

    Initial Commit

---
+++ Json_Parsing_Decodeable_Proc_Model_ObjectsTests/Json_Parsing_Decodeable_Proc_Model_ObjectsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Json_Parsing_Decodeable_Proc_Model_ObjectsTests.swift
+//  Json_Parsing_Decodeable_Proc_Model_ObjectsTests
+//
+//  Created by Daricus Duncan on 10/2/17.
+//  Copyright © 2017 DrDunkan. All rights reserved.
+//
+
+import XCTest
+@testable import Json_Parsing_Decodeable_Proc_Model_Objects
+
+class Json_Parsing_Decodeable_Proc_Model_ObjectsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
